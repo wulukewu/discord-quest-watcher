2d4d30aa2e1fc3f2f1c2c55f4d780ed60bc0056d
wulukewu <68338769+wulukewu@users.noreply.github.com>
2025-11-30 01:25:52 +0000

    chore: update quest completion script

--- scripts/CompleteDiscordQuest.js
+++ scripts/CompleteDiscordQuest.js
@@ -7,7 +7,7 @@
  * This script automates the completion of Discord quests by spoofing game activity,
  * video watching, or streaming depending on the quest requirements.
  *
- * Last synced: 2025-10-02 21:25:12 UTC
+ * Last synced: 2025-11-30 01:25:51 UTC
  */
 
 delete window.$;
